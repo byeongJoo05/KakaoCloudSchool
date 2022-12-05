16e2c99585b9187f8f73a7bc9d0fb88767d88373
SonByeongJoo <thsqudwn05@gmail.com>
2022-12-05 17:53:40 +0900

    221205

--- React/component/src/App.js
+++ React/component/src/App.js
@@ -1,9 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
+import StateComponent from './StateComponent';
 import MyComponent from './MyComponent';
-class App extends Component{
-  render(){
-    return (<div><MyComponent></MyComponent></div>)
-  }
+import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+
+function App() {
+  return (
+    <>
+    <MyComponent name= {3}>태그 안의 내용</MyComponent>
+    <StateComponent/>
+    <EventPractice/>
+    <ValidationSample/>
+    <ScrollBox ref={(ref) => this.scrollBox = ref}/>
+    <button onClick={(e)=> {this.scrollBox.scrollToBottom}}>맨 아래로</button>
+    </>
+  );
 }
 
 export default App;
+
