0542e34cb1ccff13a39dbe930cb2c57f977cd11a
SonByeongJoo <thsqudwn05@gmail.com>
2022-12-06 12:06:33 +0900

    221206

--- React/component/src/App.js
+++ React/component/src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
 import StateComponent from './StateComponent';
 import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <>
-    <MyComponent name= {3}>태그 안의 내용</MyComponent>
-    <StateComponent/>
-    <EventPractice/>
-    <ValidationSample/>
-    <ScrollBox ref={(ref) => this.scrollBox = ref}/>
-    <button onClick={(e)=> {this.scrollBox.scrollToBottom}}>맨 아래로</button>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <MyComponent name= {3}>태그 안의 내용</MyComponent>
+      <StateComponent/>
+      <EventPractice/>
+      <ValidationSample/>
+      <ScrollBox ref = {ref => {this.box = ref;}}/>
+      <button onClick={(e)=>{this.box.scrollToBottom()}}>
+        맨아래로
+      </button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ React/react_hooks/src/App.js
@@ -0,0 +1,14 @@
+import Iteration from './Iteration';
+import ErrorBoundary from './ErrorBoundary';
+
+function App() {
+  return (
+    <div>
+      <ErrorBoundary>
+      <Iteration/>
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default App;
