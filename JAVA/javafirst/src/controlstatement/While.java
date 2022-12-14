95816287652de9d93acf850ea5a40c616fcf980f
SonByeongJoo <thsqudwn05@gmail.com>
2022-12-14 16:00:11 +0900

    221214

--- JAVA/javafirst/src/controlstatement/While.java
+++ JAVA/javafirst/src/controlstatement/While.java
@@ -1,11 +1,26 @@
 package controlstatement;
 
+import java.util.Arrays;
+
 public class While {
     public static void main(String[] args) {
-        // 2차원 배열
-        String[][] programmers = {{"데니스 리치히", "비야네 스트롭스트롭", "아네르스 하일스베르"},
-                {"귀도 반 로섬", "제임스 고슬링", "켄 톰슨"}};
-        System.out.println(programmers.length); // 행의 개수
-        System.out.println(programmers[0].length); // 열의 개수
+        int[] ar = {1, 5, 3, 2, 4};
+        //선택 정렬
+        //첫번째 부터 마지막 앞 자리까지
+        for (int i = 0; i<ar.length -1; i++) {
+            // 기준의 뒤부터 마지막 자리까지
+            for(int j=i+1; j<ar.length ; j++) {
+                //자리의 데이터를 비교해서 기준 자리의 데이터가 크면
+                //2개의 데이터를 swap
+                if(ar[i] > ar[j]) {
+                    int temp = ar[i];
+                    ar[i] = ar[j];
+                    ar[j] = temp;
+                }
+            }
+            //하나의 pass가 완료되면 출력
+            System.out.println(Arrays.toString(ar));
+        }
+        System.out.println(Arrays.toString(ar));
     }
 }
