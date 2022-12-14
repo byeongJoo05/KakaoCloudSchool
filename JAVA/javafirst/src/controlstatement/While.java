015ed1ac8bc87b9e5fd5cfd0c63f7366e47f6cb3
SonByeongJoo <thsqudwn05@gmail.com>
2022-12-14 14:49:57 +0900

    221213

---
+++ JAVA/javafirst/src/controlstatement/While.java
@@ -0,0 +1,11 @@
+package controlstatement;
+
+public class While {
+    public static void main(String[] args) {
+        // 2차원 배열
+        String[][] programmers = {{"데니스 리치히", "비야네 스트롭스트롭", "아네르스 하일스베르"},
+                {"귀도 반 로섬", "제임스 고슬링", "켄 톰슨"}};
+        System.out.println(programmers.length); // 행의 개수
+        System.out.println(programmers[0].length); // 열의 개수
+    }
+}
