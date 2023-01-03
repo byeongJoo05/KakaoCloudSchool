95ed47f661e842a80c4f45a05def3846c3a88917
SonByeongJoo <thsqudwn05@gmail.com>
2023-01-03 17:30:16 +0900

    230103 SpringSTS

---
+++ SpringSTS/SpringApplicationSample/src/main/java/DiMain.java
@@ -0,0 +1,17 @@
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import di.controller.MemberController;
+
+public class DiMain {
+	public static void main(String[] args) {
+		try (GenericXmlApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml")) {
+			// Controller 가져오기
+			MemberController controller = context.getBean("memberController", MemberController.class);
+			controller.findById("1");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
