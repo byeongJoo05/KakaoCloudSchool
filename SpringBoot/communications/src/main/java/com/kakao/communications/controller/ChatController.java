64e58624ab9d43b89f02e6c2790638020808ca0b
SonByeongJoo <thsqudwn05@gmail.com>
2023-03-10 13:44:26 +0900

    230310 수업 - WebChatting - WebPush

---
+++ SpringBoot/communications/src/main/java/com/kakao/communications/controller/ChatController.java
@@ -0,0 +1,24 @@
+package com.kakao.communications.controller;
+
+import com.kakao.communications.service.WebPushService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+    @GetMapping("/")
+    public String chat() {
+        return "chatting";
+    }
+
+    private final WebPushService webPushService;
+
+    @GetMapping("/push")
+    public void push(HttpServletRequest request, HttpServletResponse response) {
+        webPushService.push(request, response);
+    }
+}
